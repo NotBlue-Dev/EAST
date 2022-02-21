43d41265fa858daecbb513a9f0dbf3d2e2d0d8ed
NotBlue-Dev <enzodubocage@gmail.com>
2022-02-21 22:17:20 +0100

    waiting + starting pas fini

--- public/starting/renderer.js
+++ public/starting/renderer.js
@@ -1,13 +1,25 @@
 let socket = io();
 
-socket.on('week', (arg) => {
-    console.log(arg)
-})
+window.addEventListener("DOMContentLoaded", (event) => {
+    const imgA = document.getElementById('teamAWait')
 
-socket.on('teams-data', (arg) => {
-    arg[0].forEach(element => {
-        const img = document.createElement("img");
-        img.src = `https://vrmasterleague.com/${element.logo}`;
-        document.body.appendChild(img);
+    const imgB = document.getElementById('teamBWait')
+
+    const week = document.getElementById('weekWait')
+
+    socket.on('week', (arg) => {
+        week.innerHTML = arg
+    })
+    
+    socket.on('teams-data', (arg) => {
+        console.log(arg)
+        imgA.src = `https://vrmasterleague.com/${arg[0].logo}`
+        imgB.src = `https://vrmasterleague.com/${arg[1].logo}`
     });
+    
+    socket.emit('get-teams-data')
+    
+    socket.emit('get-week')
+
 });
+
--- public/wait/renderer.js
+++ public/wait/renderer.js
@@ -1,21 +1,31 @@
 let socket = io();
 
-socket.on('countDownTimer', (arg) => {
-    console.log(arg)
-})
+window.addEventListener("DOMContentLoaded", (event) => {
+    const imgA = document.getElementById('teamAWait')
 
-socket.on('week', (arg) => {
-    console.log(arg)
-})
+    const imgB = document.getElementById('teamBWait')
 
-socket.on('teams-data', (arg) => {
-    // arg[0].forEach(element => {
-    //     const img = document.createElement("img");
-    //     img.src = `https://vrmasterleague.com/${element.logo}`;
-    //     document.body.appendChild(img);
-    // });
+    const timer = document.getElementById('timerWait')
+
+    const week = document.getElementById('weekWait')
+    
+    socket.on('countDownTimer', (arg) => {
+        timer.innerHTML = `${arg.m}:${arg.s}`
+    })
+    
+    socket.on('week', (arg) => {
+        week.innerHTML = arg
+    })
+    
+    socket.on('teams-data', (arg) => {
+        console.log(arg)
+        imgA.src = `https://vrmasterleague.com/${arg[0].logo}`
+        imgB.src = `https://vrmasterleague.com/${arg[1].logo}`
+    });
+    
+    socket.emit('get-teams-data')
+    
+    socket.emit('get-week')
+
 });
 
-socket.emit('get-teams-data')
-
-socket.emit('get-week')
