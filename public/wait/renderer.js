39c6fc0a431503c49de388d66a69b512979fd9df
NotBlue-Dev <enzodubocage@gmail.com>
2022-02-23 17:47:19 +0100

    waiting svg + semi responsive

--- public/wait/renderer.js
+++ public/wait/renderer.js
@@ -1,26 +1,27 @@
 let socket = io();
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    const imgA = document.getElementById('teamAWait')
+window.addEventListener("load", (event) => {
+    let svg = document.querySelector(".svgClass").getSVGDocument()
+    const imgA = svg.getElementById('teamAWait')
 
-    const imgB = document.getElementById('teamBWait')
+    const imgB = svg.getElementById('teamBWait')
 
-    const timer = document.getElementById('timerWait')
+    const timer = svg.getElementById('timer')
 
-    const week = document.getElementById('weekWait')
-    
+    const week = svg.getElementById('WEEK_')
+    console.log(imgA)
     socket.on('countDownTimer', (arg) => {
         timer.innerHTML = `${arg.m}:${arg.s}`
     })
     
     socket.on('week', (arg) => {
-        week.innerHTML = arg
+        week.innerHTML = `WEEK ${arg}`
     })
     
     socket.on('teams-data', (arg) => {
         console.log(arg)
-        imgA.src = `https://vrmasterleague.com/${arg[0].logo}`
-        imgB.src = `https://vrmasterleague.com/${arg[1].logo}`
+        imgA.setAttribute("xlink:href", `https://vrmasterleague.com/${arg[0].logo}`)
+        imgB.setAttribute("xlink:href", `https://vrmasterleague.com/${arg[1].logo}`)
     });
     
     socket.emit('get-teams-data')
