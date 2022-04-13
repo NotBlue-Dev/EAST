bb646f1398159617d52e91254b7d7e9cfe9078d7
NotBlue-Dev <enzodubocage@gmail.com>
2022-04-13 13:20:11 +0200

    shot 2/2

--- public/shot/renderer.js
+++ public/shot/renderer.js
@@ -2,46 +2,30 @@ let socket = io();
 
 window.addEventListener("load", (event) => {
 
-    // ADD SOCKET LISTENING TO CUSTOM COLOR AND DO THIS WITH THE RIGHT COLORS
-    let customMain = null
-
     const speed = document.getElementById('speed')
     const dist = document.getElementById('dist')
     const points = document.getElementById('points')
     const assist = document.getElementById('assist')
     const pseudo = document.getElementById('pseudo')
-    const number = document.getElementById('nb')
 
-    let comp = document.getElementById('component')
-
-    let svg = document.querySelector(".svgClass").getSVGDocument()
-    let listMain = svg.getElementsByClassName('MainColor')
-    for (let item of listMain) {
-        if(customMain !== null) {
-            item.style.fill = customMain
-        }
-    }
+    let comp = document.getElementById('comp')
 
     comp.classList.add('hide')
     
     socket.on('game.scoreChanged', (arg) => {
         if(arg.data.team === 'blue') {
-            svg.getElementsByClassName('teamColor')[0].style.fill = '#00A8FF'
+            document.getElementById('box').classList.add('b')
         } else {
-            svg.getElementsByClassName('teamColor')[0].style.fill = '#FFAA00'
+            document.getElementById('box').classList.add('o')
         }
         
         speed.innerHTML = `${arg.data.speed}m/s`
-        dist.innerHTML = `${arg.data.dist}M`
+        dist.innerHTML = `${arg.data.dist}m`
         points.innerHTML = arg.data.ammount
         pseudo.innerHTML = arg.data.scorer
-        let nb = arg.data.nb
-        if(nb < 10) nb = `0${nb}`
-        number.innerHTML = nb
         if(arg.data.assist !== '[INVALID]') {
-            assist.innerHTML = `assisted ${arg.data.assist}`
+            assist.innerHTML = `Assisted by ${arg.data.assist}`
         }
-
 
         comp.classList.remove('hide')
         setTimeout(() => {
