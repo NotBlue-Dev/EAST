62530ad6ffb649142ec4407b0105acbd895ce691
NotBlue-Dev <enzodubocage@gmail.com>
2022-04-20 11:42:35 +0200

    midround 1/2

--- public/betwenRound/renderer.js
+++ public/betwenRound/renderer.js
@@ -1,30 +1,6 @@
 let socket = io();
 
 window.addEventListener("load", (event) => {
-
-    // ADD SOCKET LISTENING TO CUSTOM COLOR AND DO THIS WITH THE RIGHT COLORS
-    let customMain = null
-    let customScd = null
-
-    let listMain = document.querySelector(".svgClass").getSVGDocument().getElementsByClassName('MainColor')
-    for (let item of listMain) {
-        if(customMain !== null) {
-            item.style.fill = customMain
-        }
-    }
-
-    let listScd = document.querySelector(".svgClass").getSVGDocument().getElementsByClassName('ScdColor')
-    for (let item of listScd) {
-        if(item !== null) {
-            item.style.fill = customScd
-        }
-    }
-
-
-
-
-    let svg = document.querySelector(".svgClass").getSVGDocument()
-
     const imgA = svg.getElementById('teamA')
 
     const imgB = svg.getElementById('teamB')
