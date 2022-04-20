0183eeb13df1ec6fd766fbee20d6b1b836038618
NotBlue-Dev <enzodubocage@gmail.com>
2022-04-20 20:46:21 +0200

    fix css

--- public/betwenRound/renderer.js
+++ public/betwenRound/renderer.js
@@ -1,40 +1,49 @@
 let socket = io();
 
 window.addEventListener("load", (event) => {
-    const imgA = svg.getElementById('teamA')
+    const imgA = document.getElementById('LogoA')
 
-    const imgB = svg.getElementById('teamB')
+    const imgB = document.getElementById('LogoB')
 
-    const week = svg.getElementById('WEEK_')
+    const nameA = document.getElementById('nameA')
 
-    const nameA = document.getElementById('teamNameA')
+    const nameB = document.getElementById('nameB')   
 
-    const nameB = document.getElementById('teamNameB')   
-    
-    const rankA = svg.getElementById('rankA')  
+    const fill = (arg) => {
+        orange = arg.teams.home
+        blue = arg.teams.away
 
-    const rankB = svg.getElementById('rankB')  
+        if (arg.teams.home.color !== null && arg.teams.away.color !== null) {
+            if(arg.teams.home.color === 'blue') {
+                blue = arg.teams.home
+                orange = arg.teams.away
+            } else {
+                blue = arg.teams.away
+                orange = arg.teams.home
+            }
+        }
 
-    const divA = document.getElementById('nbA')  
+        nameB.innerHTML = orange.name
+        nameA.innerHTML = blue.name
+        imgA.classList.remove('hide')
+        imgB.classList.remove('hide')
+        imgA.src = `https://vrmasterleague.com/${blue.logo}`
+        imgB.src = `https://vrmasterleague.com/${orange.logo}`
+    }
 
-    const divB = document.getElementById('nbB')  
-
-    socket.on('week', (arg) => {
-        week.innerHTML = `Week ${arg}`
-    })
-    
-    socket.on('teams-data', (arg) => {
-        nameA.innerHTML = arg[0].name
-        nameB.innerHTML = arg[1].name
-        divA.innerHTML = `${arg[0].place}th`
-        divB.innerHTML = `${arg[1].place}th`
-        rankA.setAttribute("xlink:href", `https://vrmasterleague.com/${arg[1].rank}`)
-        rankB.setAttribute("xlink:href", `https://vrmasterleague.com/${arg[0].rank}`)
-        imgA.setAttribute("xlink:href", `https://vrmasterleague.com/${arg[1].logo}`)
-        imgB.setAttribute("xlink:href", `https://vrmasterleague.com/${arg[0].logo}`)
+    socket.on('game.teamChange', (arg) => {
+        nameA.innerHTML = "ORANGE"
+        nameB.innerHTML = "BLUE"
+        imgA.classList.add('hide')
+        imgB.classList.add('hide')
     });
+
+
+    socket.on('vrml.colorChanged', fill)
+
+    socket.on('vrml.matchDataLoaded', fill)
     
-    socket.on('points', (arg) => {
+    socket.on('game.scoreBoard', (arg) => {
         for (const [key, value] of Object.entries(arg)) {
             if(value.blue !== null) {
                 document.getElementById(`${key}B`).innerHTML = value.blue
